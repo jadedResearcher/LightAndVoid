9228bf3181c473de6d5960d2f6cbdd572d83344f
JR <jadedResearcher@gmail.com>
2022-06-07 19:49:51 -0400

    init

---
+++ light.js
@@ -0,0 +1,178 @@
+const light = `
+
+<p>Nevy! &nbsp;Hi!</p>
+<p><br></p>
+<p>I feel REAAAALLY bad about the whole &apos;assuming you were dating Witherby&apos; thing. And I could tell you were getting overwhelmed trying to figure out if you even liked him like that on your own... &nbsp;</p>
+<p><br></p>
+<p>So!&nbsp;</p>
+<p><br></p>
+<p>I thought maybe I could help you out like I do with Training research?</p>
+<p><br></p>
+<p>I&apos;ve attached ALL the stuff I could find on bisexuality and how to tell if you are and all that, and I figure you can do your thing to it and figure out what parts actually matter?</p>
+<p><br></p>
+<p>Love (but not that way!)</p>
+<p><br></p>
+<p><br></p>
+<p>Devy</p>
+<p><br></p>
+<p><br></p>
+<p><br></p>
+<p><br></p>
+<p>~~~~~~~~~~~~~~~~</p>
+<p><br></p>
+<p><br></p>
+<h1>How To Tell If You (Neville) are Bisexual (for Witherby).&nbsp;</h1>
+<p>by Devona [REDACTED]</p>
+<p><br></p>
+<p><br></p>
+<p>&nbsp;</p>
+<p>Reach: This Document Should Be For Your (Neville&apos;s) Eyes Only And Not For Anyone Else (Besides Me, Devona, Who Can&apos;t Write This Blind, Ha Ha).</p>
+<p><br></p>
+<p>Risks and Opportunities: Reading This Document Risks You (Neville) Finding Out You are Bisexual (for Witherby). &nbsp;The Opportunity Is Also That You (Neville) Might Find Out You are Bisexual (for Witherby).&nbsp;</p>
+<p><br></p>
+<p>Executive Summary:&nbsp;((Nevy, I&apos;m gonna leave this blank &apos;till you work your magic in your editing pass, k?))</p>
+<p><br></p>
+<p><br></p>
+<p>Research:&nbsp;</p>
+<p><br></p>
+<p><a href="https://www.merriam-webster.com/dictionary/bisexual">https://www.merriam-webster.com/dictionary/bisexual</a></p>
+<p><br></p>
+<ul>
+    <li>
+        <p>of, relating to, or&nbsp;characterized by sexual or romantic attraction to people of one&apos;s same sex and of the opposite sex</p>
+    </li>
+    <li>
+        <p>of, relating to, or characterized by sexual or romantic attraction to people of one&apos;s own gender identity and of other gender identities</p>
+    </li>
+    <li>
+        <p>biology : possessing characters of both sexes and especially both male and female reproductive structures : HERMAPHRODITIC</p>
+    </li>
+</ul>
+<p><br></p>
+<p><a href="https://kinseyinstitute.org/research/publications/kinsey-scale.php">https://kinseyinstitute.org/research/publications/kinsey-scale.php</a></p>
+<p><br></p>
+<p>&quot;The Kinsey team interviewed thousands of people about their sexual histories.&nbsp;Research showed that sexual behavior, thoughts, and feelings towards the same or opposite sex were not always consistent across time.&nbsp;Instead of assigning people to three categories&mdash;heterosexual, bisexual, and homosexual&mdash;the team used a seven-point scale. It ranges from 0 to 6 with an additional category of &ldquo;X.&rdquo;&quot;</p>
+<p><br></p>
+<p><br></p>
+<p><br></p>
+<p><br></p>
+<p><a href="https://kinseyinstitute.org/research/publications/historical-report-diversity-of-sexual-orientation.php">https://kinseyinstitute.org/research/publications/historical-report-diversity-of-sexual-orientation.php</a></p>
+<p><br></p>
+<p>&quot;Sexual Behavior in the Human Male (1948) and Sexual Behavior in the Human Female (1953) reported that:</p>
+<p><br></p>
+<ul>
+    <li>
+        <p>37% of males&nbsp;and 13% of females&nbsp;had at least some overt homosexual experience to orgasm;</p>
+    </li>
+    <li>
+        <p>10% of males were more or less exclusively homosexual and 8% of males were exclusively homosexual for at least three years between the ages of 16 and 55. For females, Kinsey reported a range of 2-6% for more or less exclusively homosexual experience/response.</p>
+    </li>
+    <li>
+        <p>4% of males and 1-3% of females had been exclusively homosexual after the onset of adolescence up to the time of the interview.</p>
+    </li>
+</ul>
+<p><br></p>
+<p>Kinsey devised a classification scheme to measure sexual orientation. It is commonly known as the Kinsey Scale&quot;</p>
+<p><br></p>
+<p>&quot;Janus and Janus, in their&nbsp;cross-sectional (not random) nationwide&nbsp;survey of American adults&nbsp;aged 18 and over,&nbsp;stated that 9% of men&nbsp;and 5% of women&nbsp;reported having had homosexual experiences &quot;frequently&quot; or &quot;ongoing.&quot;&nbsp;In another measure, 4% of men and 2% of women self-identified as homosexual.&nbsp;The authors used a questionnaire, supplemented by 125 interviews (4,550 questionnaires were distributed, 3,260 were returned, and 2,765 were usable).&quot;<br><br></p>
+<p><br></p>
+<p>&quot;Data collected from a&nbsp;national&nbsp;sample of 13,495 men and women between 2006 and 2008. The&nbsp;study attempted to differentiate between sexual attraction, sexual behavior, and sexual identity.&nbsp;The percentage reporting their sexual identity as homosexual ranged from 2% to 4% of males, and about 1% to 2% of females.&nbsp;The percentage reporting their sexual identity as bisexual is between 1% and 3% of males, and 2% to 5% of females. About 4%&ndash;6% of males ever had same-sex contact.&nbsp;For females, the percentage who have ever had same-sex contact ranges from about 4% in the GSS, to 11%&ndash;12% in the 2002 and 2006&ndash;2008 NSFG.&quot;</p>
+<p><br></p>
+<p><br></p>
+<p><a href="https://academic.oup.com/sf/article/97/3/1067/5045222">https://academic.oup.com/sf/article/97/3/1067/5045222</a></p>
+<p><br></p>
+<p>&quot;Why do nationally representative surveys show that are there&nbsp;more&nbsp;men&nbsp;and women&nbsp;who acknowledge same-sex attractions and/or sexual practices than who identify as LGBQ&nbsp;(Copen, Chandra, and Febo-Vazquez 2016; Ward et al. 2014)?&nbsp;First, sexual orientation and sexual identity differ. Sexual orientation includes emotional attractions and sexual attractions, practices, desires, and fantasies (Savin-Williams 2014). Each element is related but distinct&nbsp;(Priebe and Svedin 2013). Aspects of orientation do not always cluster in ways suggested by common sexual identities.&quot;</p>
+<p><br></p>
+<p>&quot;A small base of qualitative research seeks to understand why&nbsp;some individuals with same-sex attractions and/or practices identify as LGBQ, whereas others identify as straight. &quot;</p>
+<p><br></p>
+<p><a href="https://www.insider.com/guides/health/sex-relationships/biromantic">https://www.insider.com/guides/health/sex-relationships/biromantic</a></p>
+<p><br></p>
+<p>&quot;Biromantic describes the capacity to be romantically attracted to more than one gender.&quot;</p>
+<p><br></p>
+<p>&quot;Being biromantic doesn&apos;t necessarily mean you&apos;re sexually attracted to people of different genders.&quot;</p>
+<p><br></p>
+<p><a href="https://www.menshealth.com/sex-women/a39431976/biromantic-meaning/">https://www.menshealth.com/sex-women/a39431976/biromantic-meaning/</a></p>
+<p><br></p>
+<p><br></p>
+<p>&quot;People often say&nbsp;&ldquo;bisexual&rdquo;&nbsp;when they&apos;re referring to both romantic and sexual attraction, but technically, it&rsquo;s a way of labeling who you&rsquo;re down to hop in the sheets with&mdash;and that&nbsp;doesn&apos;t always align with who you&apos;re romantically attracted to. For example, someone might be sexually interested in people of one gender, but romantically interested in multiple genders.&quot;</p>
+<p><br></p>
+<p>&quot; In real life, someone might be happily dating a partner they&apos;re not sexually into, but whom they love deeply. &quot;</p>
+<p><br></p>
+<p>&quot;The first step is knowing what romantic feelings feel like, and how to distinguish them from sexual and platonic feelings&mdash;which can be tricky. &quot;</p>
+<p><br></p>
+<p>&quot;&ldquo;Such activities include (but are not limited to) snuggling, holding hands, going on romantic dates, exchanging romantic words, sharing a bed, [and] being in a committed relationship,&rdquo;&nbsp;she says. She explains&nbsp;these activities will feel different from how they would with someone you want platonic intimacy with.&quot;</p>
+<p><br></p>
+<p>&quot;Ultimately, you get to decide how you identify. Figuring out who you are can take time, and that&rsquo;s OK! Try to not be too hard on yourself or rush the process.&quot;</p>
+<p><br></p>
+<p>&quot;Don&apos;t be afraid to ask questions, but also make it a priority to do your own (responsibly sourced) research.&rdquo;</p>
+<p><br></p>
+<p><br></p>
+<p><br></p>
+<p><a href="https://www.healthline.com/health/am-i-bisexual">https://www.healthline.com/health/am-i-bisexual</a></p>
+<p><br></p>
+<p><br></p>
+<p>&quot;But it can also be discombobulating. According to certified dream analyst Lauri Loewenberg, though,&nbsp;a sex dream alone isn&rsquo;t reason enough&nbsp;to get your panties in a bunch about your sexuality.&quot;</p>
+<p><br>&quot;&ldquo;The only time a sex dream may give you some inclination&nbsp;about your sexual orientation&nbsp;is if before you had the dream you were already questioning your sexuality,&rdquo;&nbsp;she says.&quot;</p>
+<p><br></p>
+<p>&quot;So, no quiz can tell you whether you&rsquo;re bisexual.</p>
+<p><br></p>
+<p>(And any quiz that claims to be able to tell your sexuality is full of sh*t!).&quot;</p>
+<p><br></p>
+<p>&ldquo;Only you can determine whether bisexual feels like a label that best fits you,&rdquo;</p>
+<p><br></p>
+<p>Bisexual activist Shiri Eisner, author of &ldquo;Bi: Notes For A Revolution,&rdquo; suggests&nbsp;asking yourself the following questions to determine if you&rsquo;re bisexual:</p>
+<p><br></p>
+<p>Does the term bisexual give me a sense of comfort?</p>
+<p>Does the term bisexual give me a sense of adventure?</p>
+<p>Is it fun for me to think about being bisexual?</p>
+<p>Does the thought of being bisexual or identifying as bisexual make me happy?</p>
+<p>Does it make me feel good about myself?</p>
+<p>Does the term bisexuality give me a sense of challenge?</p>
+<p>Does it give me community? Or support?</p>
+<p>Does it give me anything else I&rsquo;m after?</p>
+<p>If you answered yes to any of the above,&nbsp;she says: &ldquo;Just use it.&nbsp;You&rsquo;re absolutely valid.&rdquo;</p>
+<p><br></p>
+<p><br></p>
+<p><a href="https://www.cosmopolitan.com/sex-love/a27574705/am-i-bisexual/">https://www.cosmopolitan.com/sex-love/a27574705/am-i-bisexual/</a></p>
+<p><br></p>
+<p>&quot;The thing that makes bisexuality great is also what makes it kinda tragic:&nbsp;Bisexuality is subversive and hard to pin down, which means it&rsquo;s sometimes hard to actually see.&nbsp;It doesn&rsquo;t fit neatly into categories or binaries, but instead, transcends them. So there aren&rsquo;t exactly rules about what makes a person bisexual&mdash;you just sort of know it when you feel it.&quot;</p>
+<p><br></p>
+<p>&quot;If you&apos;re looking for a general definition of what it means to be bisexual, the Human Rights Campaign defines it as&nbsp;feelings of romantic or sexual attraction to more than one gender&nbsp;(meaning it could be more than two!). Where things can get murky, though, is&nbsp;when those feelings for multiple genders don&rsquo;t occur simultaneously, in the same way, or to the same degree,&nbsp;which Seattle-based relationship, intimacy, and sex therapist Claudia Johnson says&nbsp;is very common.&nbsp;That mismatch of feelings can sometimes contribute to hostility or disbelief from others about a bisexual person&rsquo;s true desires, or whether their sexuality is &ldquo;just a phase&rdquo; (eye roll) or an attempt to be &ldquo;trendy&rdquo; (double eye roll).&quot;</p>
+<p><br></p>
+<p>&quot;That&rsquo;s not to say that as a bisexual person you won&rsquo;t spend hours, if not years, of your life questioning your sexuality and wondering if you really are one thing or another&mdash;gay, straight, or maybe something else entirely. But maybe all that questioning is&nbsp;part of the fun. Or at least&nbsp;a necessary step&nbsp;on the path to enlightenment, or whatever it is you&rsquo;re going for.&quot;</p>
+<p><br></p>
+<p>&quot;There were some inklings and vibes for a while, but it didn&rsquo;t feel like it became a fact until I was&nbsp;supposed to be meeting someone for drinks and found myself&nbsp;truly flustered for the first time in a long time.&quot;</p>
+<p><br></p>
+<p>&quot;She would kiss people as a joke and&nbsp;I secretly hoped she would try to kiss me.&quot;</p>
+<p><br></p>
+<p><br></p>
+<p><br></p>
+<p>Resources:&nbsp;</p>
+<p><br></p>
+<p><a href="https://www.merriam-webster.com/dictionary/bisexual">https://www.merriam-webster.com/dictionary/bisexual</a></p>
+<p><br></p>
+<p><a href="https://www.insider.com/guides/health/sex-relationships/biromantic">https://www.insider.com/guides/health/sex-relationships/biromantic</a></p>
+<p><br></p>
+<p><br></p>
+<p><a href="https://www.healthline.com/health/am-i-bisexual">https://www.healthline.com/health/am-i-bisexual</a></p>
+<p><br></p>
+<p><a href="https://kinseyinstitute.org/research/publications/historical-report-diversity-of-sexual-orientation.php">https://kinseyinstitute.org/research/publications/historical-report-diversity-of-sexual-orientation.php</a></p>
+<p><br></p>
+<p><a href="https://www.cosmopolitan.com/sex-love/a27574705/am-i-bisexual/">https://www.cosmopolitan.com/sex-love/a27574705/am-i-bisexual/</a></p>
+<p><br></p>
+<p><a href="https://academic.oup.com/sf/article/97/3/1067/5045222">https://academic.oup.com/sf/article/97/3/1067/5045222</a></p>
+<p><br></p>
+<p><a href="https://kinseyinstitute.org/research/publications/kinsey-scale.php">https://kinseyinstitute.org/research/publications/kinsey-scale.php</a></p>
+<p><br></p>
+<p><a href="https://www.menshealth.com/sex-women/a39431976/biromantic-meaning/">https://www.menshealth.com/sex-women/a39431976/biromantic-meaning/</a></p>
+<p><br></p>
+<p><br></p>
+<p><br></p>
+<p><br></p>
+<p><br></p>
+<p><br></p>
+<p><br></p>
+<p><br></p>
+
+
+`
